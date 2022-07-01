0528a3768ab9da47a3c3e9ca97669d4debbdcb9c
Charanyash <102616426+Charanyash@users.noreply.github.com>
2022-07-01 23:52:21 +0530

    Add files via upload

---
+++ codes/rayleigh.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int main(){
+  
+   rayleigh("ray.dat",1000000);
+   // Generating rayleigh random numbers.
+   
+   return 0;
+}
